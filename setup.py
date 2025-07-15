9df8e80309d1f644401fa8ebfd6413914740be81
GuySchnidrig <g.schnidu@gmail.com>
2025-07-15 18:38:55 +0200

    update action

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "pandas>=2.0.0",
-        "numpy>=1.24.0",
+        "pandas>=2.3.1",
+        "numpy>=2.3.1",
         "duckdb>=1.3.2"
     ],
-    python_requires=">=3.9.12",
+    python_requires=">=3.11",
     description="ManaCore data pipeline package",
     author="Dr. Guy Schnidrig",
     author_email="schnidrig.guy@gmail.com",
